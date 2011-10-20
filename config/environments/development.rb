8f07ea08192482da5fbfdb29af4957aaadd8c7a8
Dan Thomas <dan.thomas@ipublic.org>
2011-10-20 13:52:02 -0400

    Add: constants for domain and command paths

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,8 @@
+UNZIP = "/usr/bin/unzip"
+OGR2OGR = "/opt/local/bin/ogr2ogr"
+OM_DOMAIN = "openmedia.dev"
+OM_PORT = 80
+
 CivicOm::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -15,6 +20,20 @@ CivicOm::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { :host => OM_DOMAIN, :port => OM_PORT }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name => "civicopenmedia@gmail.com", 
+    :password => "civic456",
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
