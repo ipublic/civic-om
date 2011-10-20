66c27eadbfaf0975a61513a95d85458d933e4dd0
Dan Thomas <dan.thomas@ipublic.org>
2011-10-20 12:23:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CivicOm::Application.config.secret_token = 'b7f6f99e0eaf696e8d6376b98d7c38aceb5f690f87ea6c8fea12c7445b00d4daf9622194ad3e32b8365469bb111768747d158947bc0e8b893e328af4c25208ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CivicOm::Application.config.session_store :cookie_store, :key => '_civic-om_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CivicOm::Application.config.session_store :active_record_store
