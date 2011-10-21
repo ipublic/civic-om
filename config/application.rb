e52c8fc26a90c5ef127e78fce366d179518ab8de
Dan Thomas <dan.thomas@ipublic.org>
2011-10-21 12:29:41 -0400

    Add: require lib/version.rb file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module CivicOm
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    
+    # config.autoload_paths += %W(#{config.root}/lib/**/)
+    require "#{config.root}/lib/version.rb"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
