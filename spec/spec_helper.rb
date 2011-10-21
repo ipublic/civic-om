03fdb5775557e00fa217467a1e0dc266c7671ccc
Dan Thomas <dan.thomas@ipublic.org>
2011-10-21 12:06:24 -0400

    Add: couchdb test and reset methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,10 +25,34 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  
+  config.before(:all) do
+    reset_test_db!
+    # TEST_SITE ||= create_test_site
+  end
+
+  config.after(:all) do
+    delete_test_db!
+    # cr = TEST_SERVER
+    # test_dbs = cr.databases.select { |db| db =~ /^#{TESTDB}/ }
+    # test_dbs.each do |db|
+    #   cr.database(db).delete! rescue nil
+    # end
+  end
 end
+
+def reset_test_db!
+  [SITE_DATABASE, STAGING_DATABASE, VOCABULARIES_DATABASE].each { |db| db.recreate! rescue nil }
+  # OpenMedia::Site.instance_variable_set(:@instance, nil)
+end
+
+def delete_test_db!
+  [SITE_DATABASE, STAGING_DATABASE, VOCABULARIES_DATABASE].each { |db| db.delete! rescue nil }
+end
+
