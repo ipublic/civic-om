76cb82be4a1198b54ad6baf2ddd89bc658cbfef5
Dan Thomas <dan.thomas@ipublic.org>
2011-10-22 23:08:52 -0400

    Fix: rename SITE_DATABASE to SITES_DATABASE

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,11 +48,11 @@ RSpec.configure do |config|
 end
 
 def reset_test_db!
-  [SITE_DATABASE, STAGING_DATABASE, VOCABULARIES_DATABASE].each { |db| db.recreate! rescue nil }
+  [SITES_DATABASE, STAGING_DATABASE, VOCABULARIES_DATABASE].each { |db| db.recreate! rescue nil }
   # OpenMedia::Site.instance_variable_set(:@instance, nil)
 end
 
 def delete_test_db!
-  [SITE_DATABASE, STAGING_DATABASE, VOCABULARIES_DATABASE].each { |db| db.delete! rescue nil }
+  [SITES_DATABASE, STAGING_DATABASE, VOCABULARIES_DATABASE].each { |db| db.delete! rescue nil }
 end
 
