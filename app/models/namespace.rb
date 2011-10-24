9f5715125afff318bc4a3cff249bc149b5f09482
Dan Thomas <dan.thomas@ipublic.org>
2011-10-24 15:55:32 -0400

    Add: addressable URI parser

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -1,18 +1,20 @@
+require "addressable/uri"
 class Namespace
 
-  attr_reader :protocol, :base_uri, :authority, :subdomain
+  attr_reader :scheme, :base_uri, :authority, :subdomain
 
   def initialize(openmedia_url)
-    pval = openmedia_url.to_s.split("://")
-    pval.length < 2 ? @protocol = "http" : @protocol = pval.first
+    uri = Addressable::URI.parse(openmedia_url.to_s)
     
-    base = openmedia_url.to_s.split("#{@protocol}://").last
-    parts = base.split('.')
-    
+    uri.scheme.nil? ? @scheme = "http" : @scheme = uri.scheme
+
+    # addressable leaves host value as nil when input scheme/protocol is missing
+    uri.host.nil? ? parts = uri.path.split('.') : parts = uri.host.split('.')
+
     # if present, shuffle the subdomain to end
     parts.length > 2 ? @subdomain = parts.delete_at(0) : @subdomain = nil
     
-    @base_uri = "#{@protocol}://#{parts.join('.')}"
+    @base_uri = "#{@scheme}://#{parts.join('.')}"
     @authority = parts.join('_')
     
     unless @subdomain.blank?
@@ -20,7 +22,7 @@ class Namespace
       @authority = @authority + "_#{@subdomain}"
     end
     
-    Hash[:prorocol => @protocool, :base_uri => @base_uri, :authority => @authority, :subdomain => @subdomain]
+    Hash[:scheme => @scheme, :base_uri => @base_uri, :authority => @authority, :subdomain => @subdomain]
   end
 
 end
