ff6bb25d618769c7317931b9eb050262441ce581
Dan Thomas <dan.thomas@ipublic.org>
2011-10-24 13:01:30 -0400

    Add: protocol instance var

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -1,15 +1,18 @@
 class Namespace
 
-  attr_reader :base_uri, :authority, :subdomain
+  attr_reader :protocol, :base_uri, :authority, :subdomain
 
   def initialize(openmedia_url)
-    base = openmedia_url.to_s.split("http://").last
+    pval = openmedia_url.to_s.split("://")
+    pval.length < 2 ? @protocol = "http" : @protocol = pval.first
+    
+    base = openmedia_url.to_s.split("#{@protocol}://").last
     parts = base.split('.')
     
     # if present, shuffle the subdomain to end
     parts.length > 2 ? @subdomain = parts.delete_at(0) : @subdomain = nil
     
-    @base_uri = "http://#{parts.join('.')}"
+    @base_uri = "#{@protocol}://#{parts.join('.')}"
     @authority = parts.join('_')
     
     unless @subdomain.blank?
@@ -17,7 +20,7 @@ class Namespace
       @authority = @authority + "_#{@subdomain}"
     end
     
-    Hash[:base_uri => @base_uri, :authority => @authority, :subdomain => @subdomain]
+    Hash[:prorocol => @protocool, :base_uri => @base_uri, :authority => @authority, :subdomain => @subdomain]
   end
 
 end
--- spec/models/namespace_spec.rb
+++ spec/models/namespace_spec.rb
@@ -2,13 +2,34 @@ require 'spec_helper'
 
 describe Namespace do
   before(:all) do
+    @url_with_ssl = "https://om.civicopenmedia.us"
+    @url_without_protocol = "om.civicopenmedia.us"
     @url_with_subdomain = "http://om.civicopenmedia.us"
     @url_without_subdomain = "http://civicopenmedia.us"
+  end
+  
+  it 'should provide proper protocol when supplied with SSL' do
+    @ns = Namespace.new(@url_with_ssl)
+    
+    @ns.protocol.should == "https"
+    @ns.base_uri.should == "https://civicopenmedia.us/om"
+    @ns.authority.should == "civicopenmedia_us_om"
+    @ns.subdomain.should == "om"
+  end
+
+  it 'should provide proper protocol when in protocol is supplid' do
+    @ns = Namespace.new(@url_without_protocol)
+
+    @ns.protocol.should == "http"
+    @ns.base_uri.should == "http://civicopenmedia.us/om"
+    @ns.authority.should == "civicopenmedia_us_om"
+    @ns.subdomain.should == "om"
   end
 
   it 'should provide proper namespace properties for URL WITH a subdomain' do
     @ns = Namespace.new(@url_with_subdomain)
 
+    @ns.protocol.should == "http"
     @ns.base_uri.should == "http://civicopenmedia.us/om"
     @ns.authority.should == "civicopenmedia_us_om"
     @ns.subdomain.should == "om"
@@ -17,6 +38,7 @@ describe Namespace do
   it 'should provide proper namespace properties for URL WITHOUT a subdomain' do
     @ns = Namespace.new(@url_without_subdomain)
 
+    @ns.protocol.should == "http"
     @ns.base_uri.should == "http://civicopenmedia.us"
     @ns.authority.should == "civicopenmedia_us"
     @ns.subdomain.should == nil
