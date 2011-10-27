2caef9572c068842fae633e99f812e8bd9f36529
Dan Thomas <dan.thomas@ipublic.org>
2011-10-27 16:09:56 -0400

    Add: public_database method

--- app/models/site.rb
+++ app/models/site.rb
@@ -7,37 +7,36 @@ class Site < CouchRest::Model::Base
   use_database SITES_DATABASE
   unique_id :authority
 
-  property :authority, String, :read_only => true
-  property :base_uri, String
+  property :name, String
   property :url, String
-
   property :public_couchhost, String
-  property :public_database, String
+  property :authority, String, :read_only => true
+  property :base_uri, String, :read_only => true
 
   timestamps!
   
   design do
+    view :by_name
     view :by_authority
     view :by_base_uri
   end
 
   # Validations
+  validates_presence_of :name
   validates_presence_of :url
+  validates_presence_of :public_couchhost
   validates_uniqueness_of :authority, :view => 'all'
 
   before_create :init_site
-
-  # def initialize(options={})
-  #   super(options)
-  #   self.url = options.delete(:url) {|key| raise ArgumentError, "you must provide a #{key} value"}
-  #   set_props
-  # end
   
-  # def couch_db
-  #   return if self.default_database.nil?
-  #   Couch::Host.new(self.default_database)
-  # end
-  # 
+  def public_database
+    return if self.authority.nil?
+    db_name = %W[#{COUCHDB_CONFIG[:db_prefix]} #{self.authority} #{COUCHDB_CONFIG[:db_suffix]}].select {|v| !v.blank?}.join('_')
+
+    server = CouchRest.new(self.url)
+    db = server.database!(db_name)
+  end
+  
   
 private
   def init_site
@@ -45,10 +44,5 @@ private
     ns = Namespace.new(self.url)
     write_attribute(:authority, ns.authority)
     write_attribute(:base_uri, ns.base_uri)
-    write_attribute(:public_couchhost, self.url)
-    write_attribute(:public_database, ns.authority)
-    
-    server = CouchRest.new(self.url)
-    db = server.database!(self.public_database)
   end
 end
