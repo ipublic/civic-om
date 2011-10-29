3d000672f2d2e92238c759e4c3454ecd718fbe81
Dan Thomas <dan.thomas@ipublic.org>
2011-10-29 07:14:56 -0400

    initial commit

---
+++ app/models/linked_data/couch_rest_model_schema.rb
@@ -0,0 +1,13 @@
+require 'open_media/couch_rest_modelable'
+module LinkedData
+  class CouchRestModelSchema < CouchRest::Model::Base
+    include ::OpenMedia::CouchRestModelable
+
+    property :identifier, String
+    property :term, String        # Escaped vocabulary name suitable for inclusion in IRI
+    property :label, String       # User assigned name, RDFS#Label
+    property :authority, String
+    property :comment, String     # RDFS#Comment
+    
+  end
+end
