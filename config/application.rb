203a5f210e2d54f27bd69f7227b55948bd4362af
Dan Thomas <dan.thomas@ipublic.org>
2011-11-19 12:54:59 -0500

    Add: autoload the lib/etl subdirectories

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,9 @@ module CivicOm
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    
-    # config.autoload_paths += %W(#{config.root}/lib/**/)
-    require "#{config.root}/lib/version.rb"
+
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/etl/**/"]  
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
