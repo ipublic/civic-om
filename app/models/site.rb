4d6f221c80c9bd61ad568a2d1dfba3f1228fefed
Dan Thomas <dan.thomas@ipublic.org>
2011-12-29 14:09:02 -0500

    Fix: inherit from CouchRest::Model::Base

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,33 +1,37 @@
 require 'no_site_defined'
 
 class Site < CouchRest::Model::Base
-
   DATABASES = [SITES_DATABASE.name, STAGING_DATABASE.name, SCHEMA_DATABASE.name]
-
   use_database SITES_DATABASE
-  unique_id :authority
+  unique_id :identifier
+  
+  belongs_to :administrator_contact, :class_name => "Vocabularies::VCard::Base"
 
-  property :term, String
-  property :label, String
-  property :authority, String, :alias => :identifier, :read_only => true
-  property :base_uri, String, :read_only => true
-  property :url, String
-  property :public_couchhost, String
+  property :identifier, String
+  property :subdomain, String, :alias => "term"
+  property :authority, String
+  property :label, String, :alias => "name"
+  property :tag_line, String
+  property :terms_of_use, String
+  # property :url, String
 
   timestamps!
   
   design do
-    view :by_label
+    view :by_subdomain
     view :by_authority
-    view :by_base_uri
+    view :by_label
   end
 
   # Validations
-  validates_presence_of :label
-  validates_presence_of :url
-  validates_presence_of :public_couchhost
-  validates_uniqueness_of :authority, :view => 'all'
+  # validates_presence_of :label
+  validates_presence_of :subdomain
+  validates_format_of :subdomain, with: /^[a-z0-9_]+$/, message: "must be lowercase alphanumerics only"
+  validates_length_of :subdomain, maximum: 32, message: "exceeds maximum of 32 characters"
+  validates_exclusion_of :subdomain, in: Subdomain::BLACK_LIST, message: "is not available"
+  validates_uniqueness_of :subdomain, :view => 'all', message: "is not available"
 
+  ## Callbacks
   before_create :init_site
   
   def public_database
@@ -38,12 +42,22 @@ class Site < CouchRest::Model::Base
     db = server.database!(db_name)
   end
   
+  def label
+    @label ||= @term.subdomain unless @subdomain.nil?
+  end
+  
+  def terms_of_use
+    self.terms_of_use ||= "By using data made available through this site the user agrees to all the conditions stated in the following paragraphs: This agency makes no claims as to the completeness, accuracy or content of any data contained in this application; makes any representation of any kind, including, but not limited to, warranty of the accuracy or fitness for a particular use; nor are any such warranties to be implied or inferred with respect to the information or data furnished herein. The data is subject to change as modifications and updates are complete. It is understood that the information obtained from this site is being used at one's own risk. These Terms of Use govern any use of this service and may be changed at any time, without notice by the sponsor agency."
+  end
   
 private
   def init_site
-    # self.url = "http://#{self.identifier}.#{OM_DOMAIN}#{OM_PORT == 80 ? '' : OM_PORT}"
-    ns = Namespace.new(self.url)
-    write_attribute(:authority, ns.authority)
-    write_attribute(:base_uri, ns.base_uri)
+    class_basename = self.class.to_s.demodulize.downcase
+    write_attribute(:identifier,  %W[#{class_basename} #{subdomain}].join('_'))
+    write_attribute(:authority, "civicopenmedia_us_#{self.term}")
+    
+    # # self.url = "http://#{self.identifier}.#{OM_DOMAIN}#{OM_PORT == 80 ? '' : OM_PORT}"
+    # ns = Namespace.new(self.url)
+    # write_attribute(:base_uri, ns.base_uri)
   end
 end
