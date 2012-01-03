28aafe488aac8811fd0f8c3b5673ea01fd1903ec
Dan Thomas <dan.thomas@ipublic.org>
2012-01-02 22:29:45 -0500

    Fix: #load_site method to work with Authority model

--- app/controllers/sites/base_controller.rb
+++ app/controllers/sites/base_controller.rb
@@ -6,27 +6,25 @@ class Sites::BaseController < ApplicationController
   SITE_NOT_DEFINED_ERROR_MSG = "Site not found."
   
   def load_site
-    return if request.subdomains.count == 0 || Subdomain::BLACK_LIST.include?(request.subdomains.first)
-    
-    ## TODO - Verify this will update if we move between subdomains 
+    authority
     current_site
     no_site_defined if @current_site.nil?
-    authority
   end    
 
-  def current_site
-    @current_site ||= Site.by_subdomain(:key => request.subdomains.first).first if request.subdomains.count > 0
+  def authority
+    @authority ||= Authority.get(params[:authority_id])
   end
   
-  def authority
-    @authority ||= @current_site.authority if @current_site
+  def current_site
+    @current_site ||= @authority.sites.first unless @authority.nil?
   end
   
   def no_site_defined
     flash[:error] = SITE_NOT_DEFINED_ERROR_MSG
     # session[:after_site_created] = params
 
-    redirect_to url_for(:host => request.domain)
+    redirect_to community_home_index_url
+    # redirect_to url_for(:host => request.domain)
   end  
   
 end
