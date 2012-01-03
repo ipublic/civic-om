c55c9d79bbc183ead9d3dfe0c4d8a7cd9dc5ae81
Dan Thomas <dan.thomas@ipublic.org>
2012-01-02 22:28:38 -0500

    Fix: disable check_for_subdomain

--- app/controllers/sites/authenticated_controller.rb
+++ app/controllers/sites/authenticated_controller.rb
@@ -2,12 +2,12 @@ class Sites::AuthenticatedController < Sites::BaseController
   layout 'sites_admin'
   
   # before_filter :authenticate_user!
-  before_filter :check_for_subdomain
+#  before_filter :check_for_subdomain
   
 
 private
   def check_for_subdomain
-    if request.subdomains.size == 0
+    if current_site.nil?
       redirect_to root_path
     elsif current_user && (current_user.site.subdomain != request.subdomains.first)
       redirect_to "#{current_user.site.url}/admin"
