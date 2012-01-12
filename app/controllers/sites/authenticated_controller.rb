80ec462d964f8457eca19aff0d1a97486f5d058f
Dan Thomas <dan.thomas@ipublic.org>
2012-01-12 09:58:45 -0500

    Fix: remove obsolete subdomain method

--- app/controllers/sites/authenticated_controller.rb
+++ app/controllers/sites/authenticated_controller.rb
@@ -2,16 +2,5 @@ class Sites::AuthenticatedController < Sites::BaseController
   layout 'sites_admin'
   
   before_filter :authenticate_user!
-#  before_filter :check_for_subdomain
-  
-
-private
-  def check_for_subdomain
-    if current_site.nil?
-      redirect_to root_path
-    elsif current_user && (current_user.site.subdomain != request.subdomains.first)
-      redirect_to "#{current_user.site.url}/admin"
-    end
-  end
   
 end
